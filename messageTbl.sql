e0344c985e12870cf284cdacc3bfefb98b3b7bd8
Elinor Azran <38763069+ElinorAzran@users.noreply.github.com>
2018-10-15 22:03:30 +0300

    Data base finished

---
+++ messageTbl.sql
@@ -0,0 +1,13 @@
+﻿create table messages(
+sn int identity(1,1), 
+idSender char(9), 
+FOREIGN KEY(idSender) REFERENCES userTbl(id),
+idReciever char(9),
+FOREIGN KEY(idReciever) REFERENCES userTbl(id),
+subject text,
+content text,
+hour time,
+dateOfMessage date,
+PRIMARY KEY(sn,idSender,idReciever)
+)
+
